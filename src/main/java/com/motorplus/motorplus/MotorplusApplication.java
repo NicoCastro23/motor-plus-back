8c8bb816e78e6775a3ee115553b4f583e390036f
Nicolas Castro <jnch2005@gmail.com>
2025-11-07 18:06:19 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/motorplus/motorplus/MotorplusApplication.java
@@ -0,0 +1,13 @@
+package com.motorplus.motorplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motorplus/motorplus/MotorplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorplus.motorplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
