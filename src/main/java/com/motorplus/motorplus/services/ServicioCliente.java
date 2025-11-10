fa4e18e4565460905e9a62cfc9538caaac700e12
Nicolas Castro <jnch2005@gmail.com>
2025-11-09 21:07:11 -0500

    Cambio de id a UUID

--- src/main/java/com/motorplus/motorplus/services/ServicioCliente.java
+++ src/main/java/com/motorplus/motorplus/services/ServicioCliente.java
@@ -7,12 +7,15 @@ import com.motorplus.motorplus.dto.vehicleDtos.VehicleCreateDto;
 import com.motorplus.motorplus.dto.vehicleDtos.VehicleDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
 public interface ServicioCliente {
     Page<ClientDto> list(String q, String email, Pageable pageable);
-    ClientDto get(Long id);
+    ClientDto get(UUID id);
     ClientDto create(ClientCreateDto dto);
-    ClientDto update(Long id, ClientUpdateDto dto);
-    void delete(Long id);
-    Page<VehicleDto> listVehicles(Long clientId, Pageable pageable);
-    VehicleDto addVehicle(Long clientId, VehicleCreateDto dto);
+    ClientDto update(UUID id, ClientUpdateDto dto);
+    void delete(UUID id);
+    Page<VehicleDto> listVehicles(UUID clientId, Pageable pageable);
+    VehicleDto addVehicle(UUID clientId, VehicleCreateDto dto);
 }
